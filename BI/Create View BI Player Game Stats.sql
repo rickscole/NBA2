6a70626597b3b5be6cb1557b3f1ebcccd1ce6ab8
rickscole <ricksaubercole@gmail.com>
2024-07-22 17:32:36 -0400

    Update Create View BI Player Game Stats.sql
    Add player season id column for BI join.

--- BI/Create View BI Player Game Stats.sql
+++ BI/Create View BI Player Game Stats.sql
@@ -3,5 +3,9 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE view [bi].[player_game_stats] as
-select * from clean.player_game_stats
+select 
+    pgs.* 
+    , player_season_id = concat(pgs.player_id, ' | ', pgs.season)
+from 
+    clean.player_game_stats pgs
 GO
