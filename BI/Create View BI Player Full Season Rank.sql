84beda290a7d918120fdbf423f3488dff1183fb0
rickscole <ricksaubercole@gmail.com>
2024-07-22 17:30:59 -0400

    Update Create View BI Player Full Season Rank.sql
    Add player season id for BI join.

--- BI/Create View BI Player Full Season Rank.sql
+++ BI/Create View BI Player Full Season Rank.sql
@@ -1,2 +1,6 @@
 create view bi.player_full_season_rank as
-select * from dbo.player_full_season_rank 
+select 
+    pfsr.* 
+    , player_season_id = concat(pfsr.player_id, ' | ', pfsr.season)
+from 
+    dbo.player_full_season_rank pfsr
