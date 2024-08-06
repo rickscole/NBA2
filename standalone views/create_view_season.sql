a7797cccee241d9a4355becb584ba3a0f82a3f11
rickscole <ricksaubercole@gmail.com>
2024-08-06 13:03:25 -0400

    Create create_view_season.sql

---
+++ standalone views/create_view_season.sql
@@ -0,0 +1,15 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create view [dbo].[season] as
+select 2015 as season 
+union select 2016
+union select 2017
+union select 2018
+union select 2019
+union select 2020
+union select 2021
+union select 2022
+union select 2023
+GO
