7a369961874b3dea3841058ada65b31fcc51fcca
rickscole <ricksaubercole@gmail.com>
2024-08-06 12:17:06 -0400

    Create create_view_regular_season_pat_rankings_superstars_and_stars.sql

---
+++ bi views/create_view_regular_season_pat_rankings_superstars_and_stars.sql
@@ -0,0 +1,7 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create view [bi].[regular_season_pat_rankings_superstars_and_stars] as
+select * from dbo.regular_season_pat_rankings_superstars_and_stars
+GO
