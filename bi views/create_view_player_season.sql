8c6f65f41f68fb95970702eb5a33bf9e7d6e102e
rickscole <ricksaubercole@gmail.com>
2024-08-26 14:48:38 -0400

    Update create_view_player_season.sql
    Add in column for player_name_season.

--- bi views/create_view_player_season.sql
+++ bi views/create_view_player_season.sql
@@ -2,12 +2,16 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE view [bi].[player_season] as
+create view [bi].[player_season] as
 select 
     distinct 
     pgs.player_id
     , pgs.season
     , player_season_id = concat(pgs.player_id, ' | ', pgs.season)
+    , pgs.player_name
+    , player_name_season = concat(player_name, ', ', season)
 from 
     clean.player_game_stats pgs
 GO
+
+
