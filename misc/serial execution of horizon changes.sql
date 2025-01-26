7ca80a3c521d246fafe666beaf7fb0c520d75096
rickscole <ricksaubercole@gmail.com>
2025-01-25 23:45:25 -0500

    Create serial execution of horizon changes.sql

---
+++ misc/serial execution of horizon changes.sql
@@ -0,0 +1,23 @@
+exec create_player_stats_horizon_changes 1	, 5
+exec create_player_stats_horizon_changes 5	, 10
+exec create_player_stats_horizon_changes 5	, 20
+exec create_player_stats_horizon_changes 5	, 41
+exec create_player_stats_horizon_changes 10, 	20
+exec create_player_stats_horizon_changes 10, 	41
+exec create_player_stats_horizon_changes 10, 	82
+exec create_player_stats_horizon_changes 20, 	41
+exec create_player_stats_horizon_changes 20, 	82
+exec create_player_stats_horizon_changes 20, 	164
+exec create_player_stats_horizon_changes 41, 	82
+exec create_player_stats_horizon_changes 41, 	164
+exec create_player_stats_horizon_changes 41, 	328
+exec create_player_stats_horizon_changes 82, 	164
+exec create_player_stats_horizon_changes 82, 	246
+exec create_player_stats_horizon_changes 82, 	328
+exec create_player_stats_horizon_changes 164, 	246
+exec create_player_stats_horizon_changes 164, 	328
+exec create_player_stats_horizon_changes 164, 	410
+exec create_player_stats_horizon_changes 246, 	492
+exec create_player_stats_horizon_changes 246, 	574
+exec create_player_stats_horizon_changes 246, 	656
+exec create_player_stats_horizon_changes 328, 	656
