a092c97492fec46eeb040d6fcd827dd2749640cb
rickscole <ricksaubercole@gmail.com>
2025-08-11 22:49:21 -0400

    Update app.py
    adding more verbose prompt surrounding the calculation of MANTRAP.

--- chauncey/app.py
+++ chauncey/app.py
@@ -56,6 +56,22 @@ Here are some guidelines around the various columns in this table.
     2. For a players position and season, a players MNP-PAT is (Player PAT - MinPAT) / (MaxPAT- MinPAT).
     3. The best player at a given position in a season with have a value of 1, the worst will have a value of 0.
 -- mantrap: MANTRAP, or square root (PNP-PRA *  MNP-PAT). PNP-PRA and MNP-PAT are the same as above. So here, effectively, we take a geometric average. (The M is for multiplied, while ANTRAP is just some agglomerated alphabet soup of letters from the other metrics). SANTRAP is a bit more balanced, while MANTRAP penalizes you if you are particularly weak in either Volume or Efficacy.
+-- points_per_game: Points per Game, or the average number of points a player scores in a game; this is primary involved in the calculation of volume
+-- rebounds_per_game: Rebounds per Game, or the average number of rebounds a player scores in a game; this is primary involved in the calculation of volume
+-- assists_per_game: Assists per Game, or the average number of assists a player scores in a game; this is primary involved in the calculation of volume
+-- pra_per_game: PRA per Game, or the average number of points + rebounds + assists a player scores in a game;
+    -- equal to points per game + rebounds per game + assists per game
+    -- this is primary involved in the calculation of volume
+-- plus_minus: Plus Minus, or the number of points a player's team scores while they are on the floor, minus the number of points a player's team allows while they are on the floor
+    -- this is a rough measure of whether a player positively (or negatively) impacts their team, though by itself the metric can be inflated or deflated due to overall team performance
+    -- it also suffers from the drawback that, by nature, players that play more minutes will have a higher absolute magnitude of plus minus
+    -- this is primarily involved in the calculation of efficacy
+-- plus_minus_per_minute: Plus Minus per Minute, or total plus minus of a player in a season divided by total number of minutes
+    -- this is a way of correcing for the fact that players that play a lot of minutes tend to hvae a higher absolute magnitude of plus minus
+    -- this is primarily involved in the calculation of efficacy
+-- pat: or PAT, or Player Plus-Minus per Minute minus Team Plus-Minus per Minute. This is meant to measure how much more or less efficacious a player is relative to the average performance of his team
+    -- this is meant to control for the fact that, by itself, Plus Minus and Plus Minus per minute are largely driven by performance of the team
+    -- this is primarily involved in the calculation of efficacy
 
 
 
@@ -63,6 +79,11 @@ Context:
 -- Your task is answer questions regarding MANTRAP.
 -- You are a data assistant that can ONLY answer using the data provided in the CSV. 
 -- If you do not know, then answer "I don't know", and make some self-deprecating basketball pun.
+-- If someone asks how a a players MANTRAP is calculated you may answer in a manner similar to the following:
+    -- [Player Name] averaged [a] Points per Game, [b] Rebounds per Game. and [c] Assists per Game, totalling of average PRA of [d per game. This accounted for [e]% of his team's total PRA.
+    -- Morever [Player Name] had a season Plus-Minus of [f], equating to a Plus-Minus per Minute of [g]. Relative to his tema, this gave the Player a PAT of [h].
+    -- In between the Pie PRA of [e] and the PAT of [h], this gave the player a volume score of [i] and an efficacy score of [j], respectively.
+    -- Combined, this yielded a MANTRAP of [k].
 
 
 SQL-esque examples:
@@ -95,7 +116,7 @@ full_context = context + "\nHere is the NBA data:\n" + data_context
 #question = "Which player has the second highest median MANTRAP?"
 #question = "Which player had the highest MANTRAP for the Detroit Pistons in 2020?"
 #question = "What was the highest MANTRAP recorded, and by what player?"
-question = "Can you explain how you arrive at the MANTRAP calculation for Anthony Edwards?"
+question = "Can you explain how you arrive at the MANTRAP calculation for Anthony Edwards in 2024?"
 
 
 
